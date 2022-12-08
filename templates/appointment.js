ef23b1bc9c89c222ef75951a55d15cbbd421abc0
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-12-08 12:12:26 +0100

    Start the css of the list

--- templates/appointment.js
+++ templates/appointment.js
@@ -2,11 +2,15 @@ var taskInput = document.getElementById('newTask');
 var addTaskButton = document.getElementById('addTaskButton');
 var incompleteTasks = document.getElementById('toDo');
 
+
 var addTask = function () {
     var text = taskInput.value;
     var li = document.createElement('li');
     li.innerHTML =
-        "<label class='appointment'>" + text + "</label>" +
+        "<span class='dateapp'>" + text + "</span>" +
+        "<span class='hourapp'>" + text + "</span>" +
+        "<label class='nameapp'>" + text + "</label>" +
+        "<button class='edit'>Edit</button>" +
         "<button class='delete'>Delete</button>";
     incompleteTasks.appendChild(li);
     taskInput.value = '';
