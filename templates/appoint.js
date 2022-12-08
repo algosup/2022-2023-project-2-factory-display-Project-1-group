491cf3f94ac0c146261c156d32030771245722b6
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-12-08 11:57:16 +0100

    Add List input

---
+++ templates/appoint.js
@@ -0,0 +1,29 @@
+var taskInput = document.getElementById('newTask');
+var addTaskButton = document.getElementById('addTaskButton');
+var incompleteTasks = document.getElementById('toDo');
+
+var addTask = function () {
+    var text = taskInput.value;
+    var li = document.createElement('li');
+    li.innerHTML =
+        "<label>" + text + "</label>" +
+        "<button class='delete'>Delete</button>";
+    incompleteTasks.appendChild(li);
+    taskInput.value = '';
+}
+
+addTaskButton.onclick = addTask;
+
+addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        addTask();
+    }
+});
+
+incompleteTasks.addEventListener('click', function (e) {
+    if (e.target.className === 'delete') {
+        var li = e.target.parentElement;
+        incompleteTasks.removeChild(li);
+    }
+});
+
