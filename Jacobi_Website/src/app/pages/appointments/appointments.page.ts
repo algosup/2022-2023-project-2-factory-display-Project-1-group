cb2523b14c7ff4b8b02a33be54dd4b3d13fc8bb1
florenthureaux <florent.hureaux@algosup.com>
2022-12-13 18:52:15 +0100

    update appointment page

--- Jacobi_Website/src/app/pages/appointments/appointments.page.ts
+++ Jacobi_Website/src/app/pages/appointments/appointments.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-appointments',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     var appointmentDate = document.getElementById('visitorDate');
@@ -49,5 +54,18 @@ addEventListener('keypress', function (e) {
 //     }
 // });
   }
+  switchPage(page: string){
+    this.authService.getCurrentUser().subscribe((user) => { // this function check if the user is already identified and redirect him to the content of the website
+      if (user) {
+        console.log('GOT USER ON LOGIN');
+        this.router.navigateByUrl(page, { replaceUrl: true });
+      }
+    });
+  }
+
+
+
+
+
 
 }
