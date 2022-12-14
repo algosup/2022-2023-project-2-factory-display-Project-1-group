e628379231c98050bfecb6b66b18a6e99002f36c
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-12-14 15:57:25 +0100

    Add custom page

--- Jacobi_Website/src/app/pages/custom-ressource/custom-ressource.page.ts
+++ Jacobi_Website/src/app/pages/custom-ressource/custom-ressource.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-custom-ressource',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-ressource.page.scss'],
 })
 export class CustomRessourcePage implements OnInit {
+  user = this.authService.getCurrentUser();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
+
+  switchPage(page: string){
+    this.authService.getCurrentUser().subscribe((user) => { // this function check if the user is already identified and redirect him to the content of the website
+      if (user) {
+        console.log('GOT USER ON LOGIN');
+        this.router.navigateByUrl(page, { replaceUrl: true });
+      }
+    });
+  }
+  signOut() {
+    this.authService.signOut();
+  }
+
 
 }
